11a0b4a630779814685330001e3a131df3863814
ibrahim-younan-1997 <ibrahimyounan1997@gmail.com>
2025-06-19 00:26:41 +0300

    update

--- res/js/script.js
+++ res/js/script.js
@@ -10,9 +10,9 @@ const App = PetiteVue.createApp({
     title: "Every-Link",
     
     menu: [
-      { name: "Homepage", icon: "home", device: "s m l", url: "/index.html", },
-      { name: "search", icon: "search", device: "s m l", url: "/search.html", },
-      { name: "tmp", icon: "link", device: "s m l", url: "/TMP.html", },
+{ name: "Homepage", icon: "home", device: "s m l", url: "/index.html", },
+{ name: "search", icon: "search", device: "s m l", url: "/search.html", },
+{ name: "tmp", icon: "link", device: "s m l", url: "/TMP.html", },
     ],
     
     
@@ -49,47 +49,47 @@ const App = PetiteVue.createApp({
   device: {
     
     get display() {
-      if (window.innerWidth < 601) { return "s" }
-      else if (window.innerWidth < 993) { return "m" }
-      else if (window.innerWidth > 993) { return "l" }
+if (window.innerWidth < 601) { return "s" }
+else if (window.innerWidth < 993) { return "m" }
+else if (window.innerWidth > 993) { return "l" }
     },
     /*________________________*/
     get screen() {
-      if (window.screen.width < 601) { return "small" }
-      else if (window.screen.width < 993) { return "medium" }
-      else if (window.screen.width > 993) { return "large" }
+if (window.screen.width < 601) { return "small" }
+else if (window.screen.width < 993) { return "medium" }
+else if (window.screen.width > 993) { return "large" }
     },
     /*________________________*/
     get type() {
-      if (/Mobile|iPhone|Android|Harmony|HUAWEI/i.test(navigator.userAgent) && window.screen.width < 601) { return "mobile" }
-      else if (/Tablet|iPad|Android|Harmony|HUAWEI/i.test(navigator.userAgent) && window.screen.width < 993) { return "tablet" }
-      else if (/Desktop|Macintosh|Mac OS X|Windows NT|X11|linux|CrOS/i.test(navigator.userAgent) && screen.width > 993) { return "desktop" }
-      else if (/SmartTV|Smart-TV|HbbTV|NetCast|Tizen|Web0S/i.test(navigator.userAgent) && window.screen.width > 993) { return "tv" }
-      else if (/Xbox|PlayStation|Nintendo/i.testnavigator.userAgent) { return "gaming" }
-      else { return "unknown" }
+if (/Mobile|iPhone|Android|Harmony|HUAWEI/i.test(navigator.userAgent) && window.screen.width < 601) { return "mobile" }
+else if (/Tablet|iPad|Android|Harmony|HUAWEI/i.test(navigator.userAgent) && window.screen.width < 993) { return "tablet" }
+else if (/Desktop|Macintosh|Mac OS X|Windows NT|X11|linux|CrOS/i.test(navigator.userAgent) && screen.width > 993) { return "desktop" }
+else if (/SmartTV|Smart-TV|HbbTV|NetCast|Tizen|Web0S/i.test(navigator.userAgent) && window.screen.width > 993) { return "tv" }
+else if (/Xbox|PlayStation|Nintendo/i.testnavigator.userAgent) { return "gaming" }
+else { return "unknown" }
     },
     /*________________________*/
     get os() {
-      if (/Android/i.test(navigator.userAgent)) { return "android" }
-      else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) { return "ios" }
-      else if (/Harmony|HUAWEI/i.test(navigator.userAgent)) { return "harmony" }
-      else if (/Windows NT/i.test(navigator.userAgent)) { return "windows" }
-      else if (/Macintosh|Mac OS X/ / i.test(navigator.userAgent)) { return "mac" }
-      else if (/Linux/i.test(navigator.userAgent)) { return "linux" }
-      else if (/CrOS/i.test(navigator.userAgent)) { return "chromeOs" }
-      else if (/KaiOS/i.test(navigator.userAgent)) { return "kaios" }
-      else if (/Tizen/i.test(navigator.userAgent)) { return "tizen" }
-      else { return "unknown" }
+if (/Android/i.test(navigator.userAgent)) { return "android" }
+else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) { return "ios" }
+else if (/Harmony|HUAWEI/i.test(navigator.userAgent)) { return "harmony" }
+else if (/Windows NT/i.test(navigator.userAgent)) { return "windows" }
+else if (/Macintosh|Mac OS X/ / i.test(navigator.userAgent)) { return "mac" }
+else if (/Linux/i.test(navigator.userAgent)) { return "linux" }
+else if (/CrOS/i.test(navigator.userAgent)) { return "chromeOs" }
+else if (/KaiOS/i.test(navigator.userAgent)) { return "kaios" }
+else if (/Tizen/i.test(navigator.userAgent)) { return "tizen" }
+else { return "unknown" }
     },
     /*________________________*/
     get browser() {
-      if (/Chrome/i.test(navigator.userAgent)) { return "chrome" }
-      else if (/Safari/i.test(navigator.userAgent)) { return "safari" }
-      else if (/Firefox/i.test(navigator.userAgent)) { return "firefox" }
-      else if (/Edg|Edge/i.test(navigator.userAgent)) { return "edge" }
-      else if (/Opera|OPR/i.test(navigator.userAgent)) { return "opera" }
-      else if (/MSIE|Trident/i.test(navigator.userAgent)) { return "IE" }
-      else { return "unknown" }
+if (/Chrome/i.test(navigator.userAgent)) { return "chrome" }
+else if (/Safari/i.test(navigator.userAgent)) { return "safari" }
+else if (/Firefox/i.test(navigator.userAgent)) { return "firefox" }
+else if (/Edg|Edge/i.test(navigator.userAgent)) { return "edge" }
+else if (/Opera|OPR/i.test(navigator.userAgent)) { return "opera" }
+else if (/MSIE|Trident/i.test(navigator.userAgent)) { return "IE" }
+else { return "unknown" }
     },
     
   }, //device
@@ -122,16 +122,25 @@ const App = PetiteVue.createApp({
     { name: "Others", id: "others", icon: "more_horiz", v: false },
   ],
   /*________________________________________________________________*/
-  //content
+  
+//tabs
+  
+  scroll() {
+    if (this.R.query) { document.querySelector(`[data-tab="${this.R.tab}"]`).scrollIntoView({ behavior: 'auto', block: 'center', inline: 'center' }); }
+  },
+  
+  /*________________________________________________________________*/
+  
+//content
   sites: null,
   
   async load() {
     if (this.R.tab != 'advanced' && this.R.tab != 'links') {
-      try { this.sites ||= await fetch(`/res/json/${this.R.tab}.json`).then(data => data.json()) } catch (error) { this.sites = null }
+try { this.sites ||= await fetch(`/res/json/${this.R.tab}.json`).then(data => data.json()) } catch (error) { this.sites = null }
     }
   },
   /*________________________________________________________________*/
-  //advanced
+//advanced
   
   advancedVisibility: JSON.parse(localStorage.getItem("advanced") || "false"),
   
@@ -149,12 +158,11 @@ const App = PetiteVue.createApp({
   },
   
   /*________________________________________________________________*/
-  //links
+//links
   
   linksVisibility: JSON.parse(localStorage.getItem("linksVisibility") || "false"),
   
   
-  edit: false,
   links: JSON.parse(localStorage.getItem('links')) || [],
   newLink: { n: "", b: "", p: "" },
   saveLinks() { localStorage.setItem('links', JSON.stringify(this.links)) },
@@ -168,77 +176,175 @@ const App = PetiteVue.createApp({
     this.newLink.p = '';
     this.saveLinks();
   },
+  /*________________________________________________________________*/
+  
+  
+  
+component: {
   
   /*________________________________________________________________*/
-  //tabs
-  
-  scroll() {
-    if (this.R.query) { document.querySelector(`[data-tab="${this.R.tab}"]`).scrollIntoView({ behavior: 'auto', block: 'center', inline: 'center' }); }
-  },
-  
-  /*________________________________________________________________*/
-  //settings
-  
-  
-  
-  visibility(name, status) { localStorage.setItem(name, JSON.stringify(status)) },
-  
-  categoriesVisibility() {
-    localStorage.setItem('categories', JSON.stringify(this.categories))
-  },
-  /*________________________________________________________________*/
-  //theme
-  
-  theme: JSON.parse(localStorage.getItem('theme') || "auto"),
-  
-  updateTheme() {
-    if (this.theme != "auto") { document.body.className = this.theme }
-  },
-  
-  changeTheme() {
+navTop(s) {return {
+
+theme: JSON.parse(localStorage.getItem('theme')) || "auto",
+changeTheme() {
     if (this.theme == "auto") { this.theme = "dark" }
     else if (this.theme === "dark") { this.theme = "light" }
     else if (this.theme === "light") { this.theme = "dark" };
     localStorage.setItem('theme', JSON.stringify(this.theme))
-    this.updateTheme();
+    if (this.theme != "auto") { document.body.className = this.theme }
   },
   
-  /*________________________________________________________________*/
-  //share
-  async share() {
-    if (!navigator.share) { ui("#share") };
-    await navigator.share({ title: document.title, url: window.location.href })
-  },
-  /*________________________________________________________________*/
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  /*________________________________________________________________*/
-  comp() {
-    return {
-      
-      $template: `
+settings: s,
 
-`
-    }
+$template: `
+
+<button class="transparent border small-round" data-ui="#drawer">
+  <i>menu</i><span>Menu</span>
+</button>
+
+<div class="max"></div>
+<h5 class="center-align" v-text="W.title"></h5>
+<div class="max"></div>
+
+<button v-show="settings !== false" class="transparent border square" data-ui="#settings">
+  <i>edit_note</i><span class="l m">Settings</span>
+</button>
+
+
+<button class="transparent border square" @click="changeTheme()">
+  <i v-text="theme === 'auto' ? 'night_sight_auto' : theme + '_mode'"></i>
+</button>
+
+`}},
+  /*________________________________________________________________*/
+share() {return {
+  
+async share() {
+  if (!navigator.share) { ui("#share") };
+  await navigator.share({ title: document.title, url: window.location.href })
   },
+
+$template: `
+
+<button class="transparent border primary-border fill small-round margin large fixed bottom right s m" @click="share" v-show="device.type === 'mobile' || device.type === 'tablet'"><i>share</i> </button>
+
+<div class="snackbar error" id="share"> Your browser is not supported</div>
+
+`}},
   /*________________________________________________________________*/
+drawer() {return {
+
+$template: `
+
+<header class="fixed">
+<nav>
+<h6 class="max lin" v-text="W.title"></h6>
+<button class="transparent border link small-round" data-ui="#drawer">
+<span>Close</span> <i>close</i>
+</button>
+</nav>
+</header>
+
+<div class="space"> </div>
+
+<ul class="list">
+
+<li v-for="link in W.menu" :class="[{'fill border primary-border': R.path === link.url}, link.device, 'wave', 'small-round']">
+<a :href="link.url + ( R.query ? '?q=' + R.query : '')">
+    <i v-text="link.icon"></i>
+    <span v-text="link.name"></span>
+</a>
+</li>
   
   
+</ul>
+
+
+`}},
+  /*________________________________________________________________*/
+
+settings() {return {
+
+visibility(name, status) { localStorage.setItem(name, JSON.stringify(status)) },
+  
+categoriesVisibility() {
+  localStorage.setItem('categories', JSON.stringify(this.categories))
+},
+
+$template: `
+
+<header class="fixed">
+<nav>
+<h6 class="max">Settings</h6>
+<button class="transparent border link small-round" data-ui="#settings">
+<span>Close</span><i>close</i>
+</button>
+</nav>
+</header>
+
+<ul class="list border">
+
+<li v-for="(cat, i) in categories" :key="cat.id">
+    <i v-text="cat.icon"></i>
+    <h6 class="small max" v-text="cat.name"></h6>
+    <label class="switch icon">
+    <input v-model="cat.v" type="checkbox" @change="categoriesVisibility()" :disabled="i === 0">
+    <span> <i>visibility_off</i> <i>visibility</i> </span>
+    </label>
+</li>
+  
+<li>
+    <i>forms_add_on</i>
+    <div class="max">
+    <h6 class="small">My Links</h6>
+    <div class="wrap">Add more websites</div>
+    </div>
+    <label class="switch icon">
+    <input v-model="linksVisibility" type="checkbox" @change="visibility('linksVisibility', linksVisibility)">
+    <span> <i>visibility_off</i> <i>visibility</i> </span>
+    </label>
+</li>
+  
+  
+<li>
+    <i>pageview</i>
+    <div class="max">
+    <h6 class="small">Advanced Search</h6>
+    <div class="wrap">Use search engines</div>
+    </div>
+    <label class="switch icon">
+    <input v-model="advancedVisibility" type="checkbox" @change="visibility('advanced', advancedVisibility)">
+    <span> <i>visibility_off</i> <i>visibility</i> </span>
+    </label>
+</li>
+  
+</ul>
+
+<div class="field label suffix border">
+  <select v-model="engine" @change="select()">
+    <option v-for="e in engines" :key="e.url" :value="e" :selected="engine.url === e.url" v-text="e.name"></option>
+  </select>
+  <label>Default Search Engine</label>
+  <i>arrow_drop_down</i>
+</div>
+
+
+`}},
+  /*________________________________________________________________*/
+
+},
+
+  
   
   /*________________________________________________________________*/
-  watch() {},
+watch() {},
   /*________________________________________________________________*/
-  mounted() {
+mounted() {
     
-    this.updateTheme();
-    
-  },
+const theme = JSON.parse(localStorage.getItem('theme')) || "auto";
+if (theme !== "auto") {document.body.className = theme};
+
+
+},
   /*________________________________________________________________*/
 }).mount("#app")
